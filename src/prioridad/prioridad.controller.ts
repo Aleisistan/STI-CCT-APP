fd20aafe478f93f8386f00f9b118f5fa9c6aafab
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-08-29 19:19:15 -0300

    cambios luego de hello world

--- src/prioridad/prioridad.controller.ts
+++ src/prioridad/prioridad.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
+//import { CreateRequerimientoDto } from './dto/CreateRequerimiento.dto';
 import { PrioridadService } from './prioridad.service';
 
 @Controller('prioridad')
 export class PrioridadController {
-    constructor(private readonly appService: PrioridadService) {}
+    constructor(private readonly PrioridadService: PrioridadService) {}
 
-    @Get('hello')
-    getHello(): string {
-      return this.appService.getHello();
+    //@Get('hello')
+    //getHello(): string {
+    //return this.PrioridadService.getHello();
+    //}
+    @Get()
+    @Render('prioridades')
+    findAll() {
+      return {prioridades: this.PrioridadService.findAll()};
     }
+    //@Get(':id')
+  //findOne(@Param() params) {
+    //return this.prioridadService.findOne(params.id);
+  //}
+
+  //@Post()
+  //create(@Body() createRequerimientoDto: CreateRequerimientoDto) {
+    //return this.prioridadService.create(createRequerimientoDto);
+  //}
+  
   }
 
--- src/prioridad/prioridad.service.ts
+++ src/prioridad/prioridad.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class PrioridadService {
+
+  @Injectable()
+  export class PrioridadService {
     getHello(): string {
-        return 'Hola Toloza!';
-      }
+      return 'Hola Toloza!';
     }
+    private readonly prioridades = [
+      {
+        "id": 1,
+        "name": "Urgente",
+        "descripción": "no enciende equipo, caídas de sistemas y redes"
+      },
+      {
+        "id": 2,
+        "name": "Intermedia",
+        "descripción": "Virus/Malwares, Desconexión intermitente de la red"
+      },
+      {
+        "id": 3,
+        "name": "Planificada",
+        "descripción": "Armado de redes, Instalación soft específicos"
+      },
+      {
+        "id": 4,
+        "name": "Mantenimiento",
+        "descripción": "Optimizacón de hardware y software, Limpieza física"
+      }
+    ];
+    findAll()  {
+      return this.prioridades;
+    }
+  };
