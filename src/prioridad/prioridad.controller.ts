3355205e6c53845e97145a21ee7b6a230e405c06
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-08-30 13:13:16 -0300

    loultimo

--- src/prioridad/prioridad.controller.ts
+++ src/prioridad/prioridad.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Render } from '@nestjs/common';
-//import { CreateRequerimientoDto } from './dto/CreateRequerimiento.dto';
+import { Body, Controller, Get, Param, Post, Render } from '@nestjs/common';
+import { CreateRequerimientosDto } from './dto/create-requerimientos.dto/create-requerimientos.dto';
 import { PrioridadService } from './prioridad.service';
 
-@Controller('prioridad')
+
+@Controller('prioridad') //ataja los pedidos y deriva desde /prioridad
 export class PrioridadController {
     constructor(private readonly PrioridadService: PrioridadService) {}
 
@@ -15,15 +16,16 @@ export class PrioridadController {
     findAll() {
       return {prioridades: this.PrioridadService.findAll()};
     }
-    //@Get(':id')
-  //findOne(@Param() params) {
-    //return this.prioridadService.findOne(params.id);
-  //}
+    @Get(':id')
+    findOne(@Param() params) {
+    return this.PrioridadService.findOne(params.id);
+  }
 
-  //@Post()
-  //create(@Body() createRequerimientoDto: CreateRequerimientoDto) {
-    //return this.prioridadService.create(createRequerimientoDto);
-  //}
+  @Post()
+  create(@Body() createRequerimientosDto: CreateRequerimientosDto) {
+  return this.PrioridadService;
+  
+  }
   
   }
 
